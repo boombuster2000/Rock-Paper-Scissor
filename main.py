c1cac4decdae87c14d7bbb718c184a1c47e59553
boombuster2000 <chanuka.williams@outlook.com>
2023-09-29 18:55:11 +0100

    Gets user input now

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from time import sleep
 from os import system
 
-moves = ["Rock", "Paper", "Scissor"]
+moves = {"Rock":"1", 
+         "Paper":"2", 
+         "Scissor":"3"}
 
 def get_menu_option():
     system("cls")
-    
+
     options = {
         "Single Player": "1",
         "Exit":"x"
@@ -19,9 +21,20 @@ def get_menu_option():
 
 def print_animation(moves):
     system("cls")
-    for move in moves:
+    for move in moves.keys():
         print(move)
         sleep(1)
+
+def get_user_move(moves):
+    system("cls")
+
+    for move in moves.keys():
+        print(f"{moves[move]}) {move}")
+
+    while True:
+        user_move = input(">> ")
+        if user_move in moves.values(): return
+
 
 while True:
     menu_option = get_menu_option()
@@ -29,3 +42,4 @@ while True:
     if menu_option == "x": break
     elif menu_option == "1": 
         print_animation(moves)
+        user_move = get_user_move(moves)
