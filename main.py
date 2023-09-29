4d4f2841c8f3447a316c9df85785997c7174ff5e
boombuster2000 <chanuka.williams@outlook.com>
2023-09-29 18:44:18 +0100

    updated animation function

--- main.py
+++ main.py
@@ -4,26 +4,23 @@ moves = ["Rock", "Paper", "Scissor"]
 def get_menu_option():
     options = {
         "Single Player": "1",
-        "Exit":"X"
+        "Exit":"x"
     }
 
     for option in options.keys(): print(f"{options[option]}) {option}")
 
     while True:
-        user_option = input(">> ")
+        user_option = input(">> ").lower()
         if user_option in options.values(): return user_option
 
-def print_animation():
-    print("Rock")
-    sleep(1)
-    print("Paper")
-    sleep(1)
-    print("Scissor")
+def print_animation(moves):
+    for move in moves:
+        print(move)
+        sleep(1)
 
 while True:
     menu_option = get_menu_option()
 
     if menu_option == "x": break
     elif menu_option == "1": 
-        print_animation()
-        
+        print_animation(moves)
